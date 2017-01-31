8e8b43426d7c50bf04c1fcd433d526bdaa8bf881
Aadith PM <aadithpmdesigns@gmail.com>
2017-01-31 18:17:52 +0530

    Added all files

---
+++ js/index.js
@@ -0,0 +1,53 @@
+function doQuote()
+{
+  $.ajax(
+    {
+      headers:
+      {
+        "X-Mashape-Key": "yfGfkMggSQmshaas2bFA2NofxGhCp1RfniNjsnrnTv4CJTbrzm",
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+      },
+      url:"https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
+      success:
+      function(reply)
+      {
+        var quote=JSON.parse(reply);
+        $("#quote-text").text(quote.quote);
+        $("#author-name").text(quote.author);
+      }
+});
+}
+
+$(document)
+  .ready(function()
+                 {
+  doQuote();
+$("footer button")
+  .click(function()
+                   {
+                    window.open
+                    ("http://github.com/aadithpm","_blank_");
+                    
+                    });
+  $(".new-button").click(function()
+                        {
+    $(".text-box-inner").animate(
+    {
+      "opacity":0
+    },600)
+    .delay(700).animate({
+      "opacity":1
+    },600);
+    
+    doQuote();
+    
+  });
+  $(".tweet-button button").click(function()
+                          {
+    var url="https://twitter.com/intent/tweet?text="+$("#quote-text").text()+'  '+"- "+$("#author-name").text();
+    console.log(url);
+    window.open(url,"_blank_");
+    
+  });
+});
